138e8fd997d251baccceeec4287fd438b44b9a69
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-01-22 14:53:36 +0530

    fifth_pattern

---
+++ Pattern_29.java
@@ -0,0 +1,19 @@
+public class Pattern_29 {
+    public static void main(String[] args) {
+        int n=5;
+        // for (int i = 1; i < n; i++) {
+        //     int k=n;
+        //     for (int j = 1; j <=n-i; j++) {
+        //         System.out.print(" "+k-- +" ");
+        //     }System.out.print(" * ");
+        //     int y=i;
+        //     for (int j = 1; j <=i; j++) {
+        //         System.out.print(" "+y-- +" ");
+        //     }
+        //     System.out.println();
+        // }
+
+
+        
+    }
+}
