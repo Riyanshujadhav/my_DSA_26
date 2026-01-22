286af9ea2e710f7097f7f603077f1e0fa023f9c6
Riyanshujadhav <riyanshujadhav864@gmail.com>
2026-01-22 13:40:10 +0530

    fourth pattern

---
+++ Pattern_23.java
@@ -0,0 +1,34 @@
+public class Pattern_23 {
+    public static void main(String[] args) {
+        int m=7;
+        int n=(m+1)/2;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j =   1; j <=n-i; j++) {
+                System.out.print(" * ");
+            }
+            for (int j =1 ; j <=2*i; j++) {
+                System.out.print("   ");    
+                
+            }
+            for (int j = 1; j <=n-i; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+        for (int i = n-1; i >= 1; i--) {
+            for (int j =   1; j <=n-i; j++) {
+                System.out.print(" * ");
+            }
+            for (int j =1 ; j <=2*i; j++) {
+                System.out.print("   ");    
+                
+            }
+            for (int j = 1; j <=n-i; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+
+}
